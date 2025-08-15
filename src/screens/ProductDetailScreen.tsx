444c9e77f6434da292273eeb6fb362e8d0066e5c
Yorland Murillo <yorlandmurillo@gmail.com>
2025-08-15 20:37:38 -0300

    fix: style in product detail

--- src/screens/ProductDetailScreen.tsx
+++ src/screens/ProductDetailScreen.tsx
@@ -8,8 +8,6 @@ import {
   FlatList,
 } from 'react-native';
 import useLoadProducts from '../hooks/useLoadProducts';
-import { Product } from '../types/product';
-import { ProductDetail } from '../types/productDetail';
 
 export default function ProductDetailScreen({ navigation, route }: any) {
   const productId = route.params.id;
@@ -20,50 +18,70 @@ export default function ProductDetailScreen({ navigation, route }: any) {
     fetchProducts(productId);
   }, []);
 
-  // const ProductDetail: React.FC<ProductDetail> = ({ product }) => {
+  if (loading) {
+    return (
+      <View style={styles.loading}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <ScrollView style={styles.container}>
-      {/* Imagen principal */}
-      <Image source={{ uri: product.thumbnail }} style={styles.image} />
+      <View style={styles.containerImg}>
+        <Image source={{ uri: product?.thumbnail }} style={styles.image} />
+      </View>
 
-      {/* Título y precio */}
-      <Text style={styles.title}>{product.title}</Text>
-      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
-      <Text style={styles.discount}>-{product.discountPercentage}% OFF</Text>
+      <Text style={styles.title}>{product?.title}</Text>
+      <Text style={styles.price}>${product?.price.toFixed(2)}</Text>
+      <Text style={styles.discount}>-{product?.discountPercentage}% OFF</Text>
 
-      {/* Descripción */}
-      <Text style={styles.sectionTitle}>Descripción</Text>
-      <Text style={styles.description}>{product.description}</Text>
+      <Text style={styles.sectionTitle}>Description</Text>
+      <Text style={styles.description}>{product?.description}</Text>
 
-      {/* Info adicional */}
-      <Text style={styles.sectionTitle}>Detalles</Text>
-      <Text>Marca: {product.brand}</Text>
-      <Text>Categoría: {product.category}</Text>
-      <Text>Stock: {product.stock}</Text>
-      <Text>Disponibilidad: {product.availabilityStatus}</Text>
-      <Text>Garantía: {product.warrantyInformation}</Text>
-      <Text>Envío: {product.shippingInformation}</Text>
-
-      {/* Reviews */}
-      <Text style={styles.sectionTitle}>Reseñas</Text>
-      <FlatList
-        data={product.reviews}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.review}>
-            <Text style={styles.reviewer}>{item.reviewerName}</Text>
-            <Text>⭐ {item.rating}</Text>
-            <Text>{item.comment}</Text>
-          </View>
-        )}
-      />
+      <Text style={styles.sectionTitle}>Details</Text>
+      <Text>
+        <Text style={styles.bold}>Brand:</Text> {product?.brand}
+      </Text>
+      <Text>
+        <Text style={styles.bold}>Category: </Text>
+        {product?.category}
+      </Text>
+      <Text>
+        <Text style={styles.bold}>Stock:</Text> {product?.stock}
+      </Text>
+      <Text>
+        <Text style={styles.bold}>Availability:</Text>{' '}
+        {product?.availabilityStatus}
+      </Text>
+      <Text>
+        <Text style={styles.bold}>Warranty: </Text>
+        {product?.warrantyInformation}
+      </Text>
+      <Text>
+        <Text style={styles.bold}>Shipping: </Text>
+        {product?.shippingInformation}
+      </Text>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, backgroundColor: '#fff' },
-  image: { width: '100%', height: 200, borderRadius: 10, marginBottom: 16 },
+  container: {
+    flex: 1,
+    flexGrow: 1,
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  containerImg: {
+    alignItems: 'center',
+  },
+  image: {
+    width: '65%',
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 16,
+  },
   title: { fontSize: 20, fontWeight: 'bold', marginBottom: 8 },
   price: { fontSize: 18, color: 'green', marginBottom: 4 },
   discount: { fontSize: 14, color: 'red', marginBottom: 16 },
@@ -80,5 +98,5 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: '#eee',
   },
-  reviewer: { fontWeight: 'bold' },
+  bold: { fontWeight: 'bold' },
 });
