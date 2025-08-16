c33a2ca8229590d1f1618cee1f1dbd2faf43a8c8
Yorland Murillo <yorlandmurillo@gmail.com>
2025-08-16 00:07:18 -0300

    feat: ask for permision in Android

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { PermissionsAndroid, Platform } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from './src/screens/Home';
 import ProductDetailScreen from './src/screens/ProductDetailScreen';
@@ -10,6 +10,33 @@ import ProductDetailScreen from './src/screens/ProductDetailScreen';
 const Stack = createStackNavigator<any>();
 
 function App() {
+  const askPermission = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.WRITE_CALENDAR,
+          PermissionsAndroid.PERMISSIONS.READ_CALENDAR,
+        ]);
+
+        const writeGranted =
+          granted[PermissionsAndroid.PERMISSIONS.WRITE_CALENDAR] ===
+          PermissionsAndroid.RESULTS.GRANTED;
+
+        const readGranted =
+          granted[PermissionsAndroid.PERMISSIONS.READ_CALENDAR] ===
+          PermissionsAndroid.RESULTS.GRANTED;
+
+        return writeGranted && readGranted;
+      } catch (err) {
+        console.warn('Permission error:', err);
+        return false;
+      }
+    }
+  };
+  useEffect(() => {
+    askPermission();
+  });
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
